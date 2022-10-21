340159dd97319fa3dcb2f038007ef6d4c48daccf
Дмитрий Горбунов <bous07@mail.ru>
2022-10-21 15:07:13 +0800

    Save code

---
+++ ServiceTelecomConnect/ServiceTelecomConnect/DataStorage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceTelecomConnect
+{
+    class DataStorage
+    {
+        public static string NameProductRepaired;
+        public static string txB_MainMeans;
+    }
+}
