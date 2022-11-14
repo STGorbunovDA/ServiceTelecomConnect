7f79986a670dc785e5d7cada5257781de5155a82
Дмитрий Горбунов <bous07@mail.ru>
2022-11-14 15:12:19 +0800

    Added methods for saving different databases. For the Directorate of Communications and the general database

--- ServiceTelecomConnect/ServiceTelecomConnect/Classes/Other/SaveFileDataGridViewPC.cs
+++ ServiceTelecomConnect/ServiceTelecomConnect/Classes/Other/SaveFileDataGridViewPC.cs
@@ -16,12 +16,15 @@ namespace ServiceTelecomConnect
         /// <summary>
         /// сохранение БД на H(S)DD
         /// </summary>
-        internal static void UserSaveFilePC(DataGridView dgw)
+        internal static void directorateSaveFilePC(DataGridView dgw, string taskcity )
         {
             try
             {
+                DateTime dateTime = DateTime.Now;
+                string dateTimeString = dateTime.ToString("dd.MM.yyyy");
                 SaveFileDialog sfd = new SaveFileDialog();
                 sfd.Filter = "csv files (*.csv)|*.csv|All files (*.*)|*.*";
+                sfd.FileName = $"База_{taskcity}_{dateTimeString}";
 
                 if (sfd.ShowDialog() == DialogResult.OK)
                 {
@@ -341,6 +344,48 @@ namespace ServiceTelecomConnect
         }
         #endregion
 
+
+        internal static void SaveFullBasePC(DataGridView dgw, string taskcity)
+        {
+            DateTime dateTime = DateTime.Now;
+            string dateTimeString = dateTime.ToString("dd.MM.yyyy");
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "csv files (*.csv)|*.csv|All files (*.*)|*.*";
+            sfd.FileName = $"ОБЩАЯ База_{taskcity}_{dateTimeString}";
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.Unicode))
+                {
+                    string note = string.Empty;
+
+                    note += $"Номер\tПолигон\tПредприятие\tМесто нахождения\tМодель\tЗаводской номер\t" +
+                        $"Инвентарный номер\tСетевой номер\tДата проведения ТО\tНомер акта\tГород\tЦена ТО\t" +
+                        $"Представитель предприятия\tДолжность\tНомер удостоверения\tДата выдачи\tНомер телефона\t" +
+                        $"Номер Акта ремонта\tКатегория\tЦена ремонта\tАнтенна\tМанипулятор\tАКБ\tЗУ\tВыполненные работы_1\t" +
+                        $"Выполненные работы_2\tВыполненные работы_3\tВыполненные работы_4\tВыполненные работы_5\t" +
+                        $"Выполненные работы_6\tВыполненные работы_7\tДеталь_1\tДеталь_2\tДеталь_3\tДеталь_4\tДеталь_5\t" +
+                        $"Деталь_6\tДеталь_7\t№ Акта списания\tПримечания";
+
+                    sw.WriteLine(note);
+
+                    for (int i = 0; i < dgw.Rows.Count; i++)
+                    {
+                        for (int j = 0; j < dgw.ColumnCount; j++)
+                        {
+                            var re = new Regex(Environment.NewLine);
+                            var value = dgw.Rows[i].Cells[j].Value.ToString();
+                            value = re.Replace(value, " ");
+
+                            sw.Write(value + "\t");//todo решить
+                        }
+                        sw.WriteLine();
+                    }
+
+                }
+            }
+        }
+
         #region Сохранение Бд по Таймеру 
 
         internal static void AutoSaveFilePC(DataGridView dgw, string city)
@@ -380,7 +425,10 @@ namespace ServiceTelecomConnect
                     {
                         for (int j = 0; j < dgw.ColumnCount; j++)
                         {
-                            sw.Write(dgw.Rows[i].Cells[j].Value.ToString() + "\t");//todo решить
+                            var re = new Regex(Environment.NewLine);
+                            var value = dgw.Rows[i].Cells[j].Value.ToString();
+                            value = re.Replace(value, " ");
+                            sw.Write(value + "\t");//todo решить
                         }
                         sw.WriteLine();
                     }
@@ -425,7 +473,10 @@ namespace ServiceTelecomConnect
                     {
                         for (int j = 0; j < dgw.ColumnCount; j++)
                         {
-                            sw.Write(dgw.Rows[i].Cells[j].Value.ToString() + "\t");//todo решить
+                            var re = new Regex(Environment.NewLine);
+                            var value = dgw.Rows[i].Cells[j].Value.ToString();
+                            value = re.Replace(value, " ");
+                            sw.Write(value + "\t");//todo решить
                         }
                         sw.WriteLine();
                     }
