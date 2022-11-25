12155c75cf9e5ed51502bb4770160d7f13efbee1
Дмитрий Горбунов <bous07@mail.ru>
2022-11-25 16:53:51 +0800

    Refactoring code

--- ServiceTelecomConnect/ServiceTelecomConnect/Forms/Setting_user.cs
+++ ServiceTelecomConnect/ServiceTelecomConnect/Forms/Setting_user.cs
@@ -179,16 +179,21 @@ namespace ServiceTelecomConnect
                                 var id = Convert.ToInt32(dataGridView1.Rows[index].Cells[0].Value);
                                 var login = dataGridView1.Rows[index].Cells[1].Value;
                                 var deleteQuery = $"delete from users where id = {id}";
-                                var delete_сharacteristics_вrigade = $"delete from сharacteristics_вrigade where section_foreman_FIO = '{login}' " +
-                                    $"OR engineers_FIO = '{login}' OR curator = '{login}' OR departmentCommunications = '{login}'";
+
+                                var update_сharacteristics_вrigade = $"UPDATE сharacteristics_вrigade SET section_foreman_FIO = '' " +
+                                    $"OR engineers_FIO = '' OR curator = '' OR departmentCommunications = '' " +
+                                    $"WHERE section_foreman_FIO = '{login}' OR engineers_FIO = '{login}' OR curator = '{login}' " +
+                                    $"OR departmentCommunications = '{login}'";
+                                
                                 using (MySqlCommand command = new MySqlCommand(deleteQuery, DB.GetInstance.GetConnection()))
                                 {
                                     command.ExecuteNonQuery();
                                 }
-                                using (MySqlCommand command2 = new MySqlCommand(delete_сharacteristics_вrigade, DB.GetInstance.GetConnection()))
+                                using (MySqlCommand command2 = new MySqlCommand(update_сharacteristics_вrigade, DB.GetInstance.GetConnection()))
                                 {
                                     command2.ExecuteNonQuery();
                                 }
+
                             }
                         }
                         DB.GetInstance.CloseConnection();
