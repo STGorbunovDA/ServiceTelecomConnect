7b58d2944d479b8a5227c27533c0441d0cbd6ce3
Дмитрий Горбунов <bous07@mail.ru>
2022-11-26 19:42:28 +0800

    Refactoring code

--- ServiceTelecomConnect/ServiceTelecomConnect/Forms/AddRSTForm.cs
+++ ServiceTelecomConnect/ServiceTelecomConnect/Forms/AddRSTForm.cs
@@ -5,7 +5,6 @@ using System.Globalization;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
 
 namespace ServiceTelecomConnect
 {
