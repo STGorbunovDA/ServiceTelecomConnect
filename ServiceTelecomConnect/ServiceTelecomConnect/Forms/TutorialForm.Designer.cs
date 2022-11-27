d21a4e62a3b1d8a234b085808f2a658a75a5e6d0
Дмитрий Горбунов <bous07@mail.ru>
2022-11-28 06:13:10 +0800

    Refactoring code

--- ServiceTelecomConnect/ServiceTelecomConnect/Forms/TutorialForm.Designer.cs
+++ ServiceTelecomConnect/ServiceTelecomConnect/Forms/TutorialForm.Designer.cs
@@ -34,50 +34,120 @@
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle10 = new System.Windows.Forms.DataGridViewCellStyle();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.cmB_model = new System.Windows.Forms.ComboBox();
+            this.cmb_unique = new System.Windows.Forms.ComboBox();
+            this.cmB_seach = new System.Windows.Forms.ComboBox();
+            this.btn_new_rst_problem = new System.Windows.Forms.Button();
+            this.picB_update = new System.Windows.Forms.PictureBox();
+            this.btn_search = new System.Windows.Forms.Button();
+            this.txB_search = new System.Windows.Forms.TextBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picB_update)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.BackgroundImage = global::ServiceTelecomConnect.Properties.Resources._999;
-            this.panel1.Controls.Add(this.cmB_model);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.cmb_unique);
+            this.panel1.Controls.Add(this.cmB_seach);
+            this.panel1.Controls.Add(this.btn_new_rst_problem);
+            this.panel1.Controls.Add(this.picB_update);
+            this.panel1.Controls.Add(this.btn_search);
+            this.panel1.Controls.Add(this.txB_search);
             this.panel1.Location = new System.Drawing.Point(1, 3);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1293, 100);
+            this.panel1.Size = new System.Drawing.Size(1349, 92);
             this.panel1.TabIndex = 0;
             // 
-            // cmB_model
+            // cmb_unique
             // 
-            this.cmB_model.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cmB_model.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.cmB_model.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.cmB_model.FormattingEnabled = true;
-            this.cmB_model.Items.AddRange(new object[] {
-            "Восточно-Сибирская ЖД",
-            "Горьковская ЖД",
-            "Дальневосточная ЖД",
-            "Забайкальская ЖД",
-            "Западно-Сибирская ЖД",
-            "Калининградская ЖД",
-            "Красноярская ЖД",
-            "Куйбышевская ЖД",
-            "Московская ЖД",
-            "Октябрьская ЖД",
-            "Приволжская ЖД",
-            "Сахалинская ЖД",
-            "Свердловская ЖД",
-            "Северная ЖД",
-            "Северо-Кавказская ЖД",
-            "Юго-Восточная ЖД",
-            "Южно-Уральская ЖД"});
-            this.cmB_model.Location = new System.Drawing.Point(11, 9);
-            this.cmB_model.Name = "cmB_model";
-            this.cmB_model.Size = new System.Drawing.Size(235, 28);
-            this.cmB_model.TabIndex = 83;
-            this.cmB_model.SelectionChangeCommitted += new System.EventHandler(this.CmB_model_SelectionChangeCommitted);
+            this.cmb_unique.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmb_unique.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmb_unique.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cmb_unique.FormattingEnabled = true;
+            this.cmb_unique.Items.AddRange(new object[] {
+            "Модель",
+            "Неисправность",
+            "Автор"});
+            this.cmb_unique.Location = new System.Drawing.Point(245, 10);
+            this.cmb_unique.Name = "cmb_unique";
+            this.cmb_unique.Size = new System.Drawing.Size(292, 28);
+            this.cmb_unique.TabIndex = 95;
+            this.cmb_unique.Visible = false;
+            this.cmb_unique.SelectedIndexChanged += new System.EventHandler(this.Cmb_unique_SelectedIndexChanged);
+            // 
+            // cmB_seach
+            // 
+            this.cmB_seach.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmB_seach.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmB_seach.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cmB_seach.FormattingEnabled = true;
+            this.cmB_seach.Items.AddRange(new object[] {
+            "Модель",
+            "Неисправность",
+            "Автор",
+            "Виды работ"});
+            this.cmB_seach.Location = new System.Drawing.Point(11, 10);
+            this.cmB_seach.Name = "cmB_seach";
+            this.cmB_seach.Size = new System.Drawing.Size(228, 28);
+            this.cmB_seach.TabIndex = 94;
+            this.cmB_seach.SelectionChangeCommitted += new System.EventHandler(this.Cmb_seach_SelectionChangeCommitted);
+            // 
+            // btn_new_rst_problem
+            // 
+            this.btn_new_rst_problem.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_new_rst_problem.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_new_rst_problem.Location = new System.Drawing.Point(245, 44);
+            this.btn_new_rst_problem.Name = "btn_new_rst_problem";
+            this.btn_new_rst_problem.Size = new System.Drawing.Size(292, 31);
+            this.btn_new_rst_problem.TabIndex = 93;
+            this.btn_new_rst_problem.Text = "Добавить новую неисправность";
+            this.btn_new_rst_problem.UseVisualStyleBackColor = false;
+            this.btn_new_rst_problem.Click += new System.EventHandler(this.Btn_new_rst_problem_Click);
+            // 
+            // picB_update
+            // 
+            this.picB_update.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picB_update.BackColor = System.Drawing.Color.Transparent;
+            this.picB_update.BackgroundImage = global::ServiceTelecomConnect.Properties.Resources.system_software_update_22485__1_;
+            this.picB_update.Location = new System.Drawing.Point(592, 10);
+            this.picB_update.Name = "picB_update";
+            this.picB_update.Size = new System.Drawing.Size(50, 48);
+            this.picB_update.TabIndex = 92;
+            this.picB_update.TabStop = false;
+            this.picB_update.Click += new System.EventHandler(this.PicB_update_Click);
+            // 
+            // btn_search
+            // 
+            this.btn_search.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_search.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_search.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_search.Location = new System.Drawing.Point(543, 10);
+            this.btn_search.Name = "btn_search";
+            this.btn_search.Size = new System.Drawing.Size(29, 29);
+            this.btn_search.TabIndex = 91;
+            this.btn_search.Text = "...";
+            this.btn_search.UseVisualStyleBackColor = false;
+            this.btn_search.Click += new System.EventHandler(this.Btn_search_Click);
+            // 
+            // txB_search
+            // 
+            this.txB_search.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txB_search.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txB_search.Location = new System.Drawing.Point(245, 10);
+            this.txB_search.Name = "txB_search";
+            this.txB_search.Size = new System.Drawing.Size(292, 29);
+            this.txB_search.TabIndex = 89;
+            this.txB_search.DoubleClick += new System.EventHandler(this.TxB_search_DoubleClick);
+            this.txB_search.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxB_search_KeyPress);
             // 
             // dataGridView1
             // 
@@ -85,14 +155,13 @@
             this.dataGridView1.AllowUserToDeleteRows = false;
             dataGridViewCellStyle6.BackColor = System.Drawing.Color.White;
             this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle6;
-            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridView1.BackgroundColor = System.Drawing.Color.SkyBlue;
             this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
             dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
             dataGridViewCellStyle7.BackColor = System.Drawing.Color.Black;
-            dataGridViewCellStyle7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            dataGridViewCellStyle7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             dataGridViewCellStyle7.ForeColor = System.Drawing.SystemColors.Window;
             dataGridViewCellStyle7.SelectionBackColor = System.Drawing.SystemColors.Highlight;
             dataGridViewCellStyle7.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
@@ -100,38 +169,50 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
             dataGridViewCellStyle8.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            dataGridViewCellStyle8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             dataGridViewCellStyle8.ForeColor = System.Drawing.SystemColors.ControlText;
             dataGridViewCellStyle8.SelectionBackColor = System.Drawing.SystemColors.Highlight;
             dataGridViewCellStyle8.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
             dataGridViewCellStyle8.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
             this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle8;
             this.dataGridView1.GridColor = System.Drawing.SystemColors.ActiveCaption;
-            this.dataGridView1.Location = new System.Drawing.Point(1, 109);
+            this.dataGridView1.Location = new System.Drawing.Point(1, 101);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RightToLeft = System.Windows.Forms.RightToLeft.No;
             dataGridViewCellStyle9.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle9.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            dataGridViewCellStyle9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            dataGridViewCellStyle9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             dataGridViewCellStyle9.ForeColor = System.Drawing.SystemColors.WindowText;
             dataGridViewCellStyle9.SelectionBackColor = System.Drawing.SystemColors.Highlight;
             dataGridViewCellStyle9.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
             dataGridViewCellStyle9.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
             this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle9;
-            dataGridViewCellStyle10.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle10.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle10;
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dataGridView1.ShowCellToolTips = false;
-            this.dataGridView1.Size = new System.Drawing.Size(1293, 321);
+            this.dataGridView1.Size = new System.Drawing.Size(1349, 548);
             this.dataGridView1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(11, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(228, 31);
+            this.button1.TabIndex = 96;
+            this.button1.Text = "Краткая иформация";
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // TutorialForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.Color.SkyBlue;
-            this.ClientSize = new System.Drawing.Size(1296, 567);
+            this.ClientSize = new System.Drawing.Size(1352, 661);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -139,6 +220,8 @@
             this.Text = "Обучалка";
             this.Load += new System.EventHandler(this.TutorialForm_Load);
             this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picB_update)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
@@ -147,7 +230,13 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.ComboBox cmB_model;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txB_search;
+        private System.Windows.Forms.Button btn_search;
+        private System.Windows.Forms.PictureBox picB_update;
+        private System.Windows.Forms.Button btn_new_rst_problem;
+        private System.Windows.Forms.ComboBox cmB_seach;
+        private System.Windows.Forms.ComboBox cmb_unique;
+        private System.Windows.Forms.Button button1;
     }
 }
--- ServiceTelecomConnect/ServiceTelecomConnect/Forms/TutorialForm.cs
+++ ServiceTelecomConnect/ServiceTelecomConnect/Forms/TutorialForm.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ServiceTelecomConnect.Forms
@@ -13,8 +7,6 @@ namespace ServiceTelecomConnect.Forms
     public partial class TutorialForm : Form
     {
         private readonly cheakUser _user;
-
-        DB dB = new DB();
 
         int selectedRow;
 
@@ -27,11 +19,11 @@ namespace ServiceTelecomConnect.Forms
             this.dataGridView1.RowsDefaultCellStyle.BackColor = Color.GhostWhite;
             this.dataGridView1.AlternatingRowsDefaultCellStyle.BackColor = Color.White;
             _user = user;
+            cmB_seach.Text = cmB_seach.Items[0].ToString();
         }
 
         void TutorialForm_Load(object sender, EventArgs e)
         {
-            QuerySettingDataBase.ModelGetEngineer(cmB_model);
             dataGridView1.EnableHeadersVisualStyles = false;
             dataGridView1.ColumnHeadersDefaultCellStyle.Font = new Font(dataGridView1.ColumnHeadersDefaultCellStyle.Font.FontFamily, 12f, FontStyle.Bold); //жирный курсив размера 16
             dataGridView1.ColumnHeadersDefaultCellStyle.BackColor = Color.White; //цвет текста
@@ -44,9 +36,122 @@ namespace ServiceTelecomConnect.Forms
             dataGridView1.AllowUserToResizeRows = false;
         }
 
-        void CmB_model_SelectionChangeCommitted(object sender, EventArgs e)
+
+        void PicB_update_Click(object sender, EventArgs e)
         {
+            if (dataGridView1.Rows.Count == 0)
+            {
+                MessageBox.Show("Сначала добавь неисправность", "Отмена", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
             QuerySettingDataBase.RefreshDataGridEngineer(dataGridView1);
+        }
+
+        void Btn_new_rst_problem_Click(object sender, EventArgs e)
+        {
+            if (Internet_check.CheackSkyNET())
+            {
+                try
+                {
+                    AddToProblemRST addProblemRST = new AddToProblemRST(_user);
+                    if (Application.OpenForms["AddToProblemRST"] == null)
+                    {
+                        addProblemRST.DoubleBufferedForm(true);
+                        addProblemRST.Show();
+                    }
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Ошибка создания формы AddRSTForm(Btn_new_rst_problem_Click)");
+                }
+            }    
+        }
+
+        void Cmb_seach_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            if (cmB_seach.SelectedIndex == 0)
+            {
+                try
+                {
+                    cmb_unique.Visible = true;
+                    txB_search.Visible = false;
+                    txB_search.Clear();
+                    QuerySettingDataBase.Cmb_unique_model_engineer(cmb_unique);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка! Модели не добавлены в comboBox!");
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+            if (cmB_seach.SelectedIndex == 1)
+            {
+                try
+                {
+                    cmb_unique.Visible = true;
+                    txB_search.Visible = false;
+                    txB_search.Clear();
+                    QuerySettingDataBase.Cmb_unique_problem_engineer(cmb_unique);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка! неисправности не добавлены в comboBox!");
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+            if (cmB_seach.SelectedIndex == 2)
+            {
+                try
+                {
+                    cmb_unique.Visible = true;
+                    txB_search.Visible = false;
+                    txB_search.Clear();
+                    QuerySettingDataBase.Cmb_unique_author_engineer(cmb_unique);
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка! Авторы не добавлены в comboBox!");
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+            if (cmB_seach.SelectedIndex == 3)
+            {
+                try
+                {
+                    txB_search.Visible = true;
+                    cmb_unique.Visible = false;
+                   
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка! Авторы не добавлены в comboBox!");
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+        }
+
+        void Cmb_unique_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            QuerySettingDataBase.SearchEngineer(dataGridView1, cmB_seach.Text, txB_search.Text, cmb_unique.Text);
+        }
+
+        void Btn_search_Click(object sender, EventArgs e)
+        {
+            QuerySettingDataBase.SearchEngineer(dataGridView1, cmB_seach.Text, txB_search.Text, cmb_unique.Text);
+        }
+
+        void TxB_search_DoubleClick(object sender, EventArgs e)
+        {
+            QuerySettingDataBase.SearchEngineer(dataGridView1, cmB_seach.Text, txB_search.Text, cmb_unique.Text);
+        }
+
+        private void TxB_search_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Return)
+            {
+                QuerySettingDataBase.SearchEngineer(dataGridView1, cmB_seach.Text, txB_search.Text, cmb_unique.Text);
+            }
         }
     }
 }
