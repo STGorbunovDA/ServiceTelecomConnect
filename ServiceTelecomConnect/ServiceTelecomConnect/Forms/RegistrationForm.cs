85aa02d182882880f60fb171c1cba84e6da9a1bc
STGorbunovDA <bous07@mail.ru>
2022-12-04 23:52:31 +0800

    Refactoring code

--- ServiceTelecomConnect/ServiceTelecomConnect/Forms/RegistrationForm.cs
+++ ServiceTelecomConnect/ServiceTelecomConnect/Forms/RegistrationForm.cs
@@ -69,7 +69,7 @@ namespace ServiceTelecomConnect
                 if (Internet_check.CheackSkyNET())
                 {
                     var loginUser = txB_loginField.Text;
-                    var passUser = Md5.hashPassword(txB_passField.Text);
+                    var passUser = Md5.EncryptPlainTextToCipherText(txB_passField.Text);
 
                     if (!CheackUser(loginUser, passUser))
                     {
