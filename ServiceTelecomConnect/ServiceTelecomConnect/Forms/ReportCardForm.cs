3e3d69dd43733a8d4e9e9c77f4322e52ceaa5e2a
STGorbunovDA <bous07@mail.ru>
2022-12-07 04:33:49 +0800

    Creating an employee tabulation form

---
+++ ServiceTelecomConnect/ServiceTelecomConnect/Forms/ReportCardForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServiceTelecomConnect.Forms
+{
+    public partial class ReportCardForm : Form
+    {
+        public ReportCardForm()
+        {
+            StartPosition = FormStartPosition.CenterScreen;
+            InitializeComponent();
+        }
+
+        void ReportCardForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
