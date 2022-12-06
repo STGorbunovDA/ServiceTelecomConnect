a19b0fa20f52b5ed9248b1a6c055db719c004997
STGorbunovDA <bous07@mail.ru>
2022-12-07 04:32:51 +0800

    Refactoring code

--- ServiceTelecomConnect/ServiceTelecomConnect/Forms/Setting_user.cs
+++ ServiceTelecomConnect/ServiceTelecomConnect/Forms/Setting_user.cs
@@ -34,71 +34,51 @@ namespace ServiceTelecomConnect
 
         private void CreateColums()
         {
-            try
-            {
-                dataGridView1.Columns.Add("id", "№");
-                dataGridView1.Columns.Add("login", "Логин");
-                dataGridView1.Columns.Add("pass", "Пароль");
-                dataGridView1.Columns.Add("is_admin", "Должность");
-                dataGridView1.Columns.Add("IsNew", String.Empty);
-                dataGridView1.Columns[4].Visible = false;
-            }
-            catch (Exception)
-            {
-                MessageBox.Show("Ошибка! Не сформированы столбцы Datagrid(CreateColums)");
-            }
+            dataGridView1.Columns.Add("id", "№");
+            dataGridView1.Columns.Add("login", "Логин");
+            dataGridView1.Columns.Add("pass", "Пароль");
+            dataGridView1.Columns.Add("is_admin", "Должность");
+            dataGridView1.Columns.Add("IsNew", String.Empty);
+            dataGridView1.Columns[4].Visible = false;
         }
 
         void ReedSingleRow(DataGridView dgw, IDataRecord record)
         {
-            try
-            {
-                dataGridView1.Invoke((MethodInvoker)(() => dgw.Rows.Add(record.GetInt32(0), record.GetString(1), record.GetString(2), record.GetString(3), RowState.ModifieldNew)));
-            }
-            catch (Exception)
-            {
-                MessageBox.Show("Ошибка! Не загруженны данные в Datagridview(ReedSingleRow)");
-            }
+            dataGridView1.Invoke((MethodInvoker)(() => dgw.Rows.Add(record.GetInt32(0), record.GetString(1),
+                record.GetString(2), record.GetString(3), RowState.ModifieldNew)));
         }
 
         void RefreshDataGrid(DataGridView dgw)
         {
             if (Internet_check.CheackSkyNET())
             {
-                try
+                var myCulture = new CultureInfo("ru-RU");
+                myCulture.NumberFormat.NumberDecimalSeparator = ".";
+                Thread.CurrentThread.CurrentCulture = myCulture;
+                dgw.Rows.Clear();
+
+                string queryString = $"select id, login, pass, is_admin from users";
+
+                using (MySqlCommand command = new MySqlCommand(queryString, DB.GetInstance.GetConnection()))
                 {
-                    var myCulture = new CultureInfo("ru-RU");
-                    myCulture.NumberFormat.NumberDecimalSeparator = ".";
-                    Thread.CurrentThread.CurrentCulture = myCulture;
-                    dgw.Rows.Clear();
+                    DB.GetInstance.OpenConnection();
 
-                    string queryString = $"select id, login, pass, is_admin from users";
-
-                    using (MySqlCommand command = new MySqlCommand(queryString, DB.GetInstance.GetConnection()))
+                    using (MySqlDataReader reader = command.ExecuteReader())
                     {
-                        DB.GetInstance.OpenConnection();
-
-                        using (MySqlDataReader reader = command.ExecuteReader())
+                        if (reader.HasRows)
                         {
-                            if (reader.HasRows)
+                            while (reader.Read())
                             {
-                                while (reader.Read())
-                                {
-                                    ReedSingleRow(dgw, reader);
-                                }
-                                reader.Close();
+                                ReedSingleRow(dgw, reader);
                             }
+                            reader.Close();
                         }
-                        command.ExecuteNonQuery();
-                        DB.GetInstance.CloseConnection();
                     }
-                    dataGridView1.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
-                    dataGridView1.AutoResizeRows(DataGridViewAutoSizeRowsMode.AllCells);
+                    command.ExecuteNonQuery();
+                    DB.GetInstance.CloseConnection();
                 }
-                catch (Exception)
-                {
-                    MessageBox.Show("Ошибка! Не загруженны данные в Datagridview(RefreshDataGrid)");
-                }
+                dataGridView1.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
+                dataGridView1.AutoResizeRows(DataGridViewAutoSizeRowsMode.AllCells);
             }
         }
         void Setting_user_Load(object sender, EventArgs e)
@@ -112,25 +92,17 @@ namespace ServiceTelecomConnect
 
         void DataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-            try
-            {
-                dataGridView1.ReadOnly = false;
+            dataGridView1.ReadOnly = false;
 
-                selectedRow = e.RowIndex;
-                if (e.RowIndex >= 0)
-                {
-                    DataGridViewRow row = dataGridView1.Rows[selectedRow];
-                    txB_id.Text = row.Cells[0].Value.ToString();
-                    txB_login.Text = row.Cells[1].Value.ToString();
-                  //txB_pass.Text = row.Cells[2].Value.ToString();
-                    txB_pass.Text = Md5.DecryptCipherTextToPlainText(row.Cells[2].Value.ToString());
-                    cmB_is_admin_post.Text = row.Cells[3].Value.ToString();
-                }
-            }
-            catch (Exception ex)
+            selectedRow = e.RowIndex;
+            if (e.RowIndex >= 0)
             {
-                MessageBox.Show(ex.ToString());
-                MessageBox.Show("Ошибка! (DataGridView1_CellClick)");
+                DataGridViewRow row = dataGridView1.Rows[selectedRow];
+                txB_id.Text = row.Cells[0].Value.ToString();
+                txB_login.Text = row.Cells[1].Value.ToString();
+                //txB_pass.Text = row.Cells[2].Value.ToString();
+                txB_pass.Text = Md5.DecryptCipherTextToPlainText(row.Cells[2].Value.ToString());
+                cmB_is_admin_post.Text = row.Cells[3].Value.ToString();
             }
         }
 
@@ -146,14 +118,7 @@ namespace ServiceTelecomConnect
         {
             if (Internet_check.CheackSkyNET())
             {
-                try
-                {
-                    RefreshDataGrid(dataGridView1);
-                }
-                catch (Exception)
-                {
-                    MessageBox.Show("Ошибка! Не загруженны данные в Datagridview(RefreshDataGrid)");
-                }
+                RefreshDataGrid(dataGridView1);
             }
         }
 
@@ -161,79 +126,65 @@ namespace ServiceTelecomConnect
         {
             if (Internet_check.CheackSkyNET())
             {
-                try
+                foreach (DataGridViewRow row in dataGridView1.SelectedRows)
                 {
-                    foreach (DataGridViewRow row in dataGridView1.SelectedRows)
-                    {
-                        dataGridView1.Rows[row.Index].Cells[4].Value = RowState.Deleted;
-                    }
-                    if (Internet_check.CheackSkyNET())
-                    {
+                    dataGridView1.Rows[row.Index].Cells[4].Value = RowState.Deleted;
+                }
+                if (Internet_check.CheackSkyNET())
+                {
 
-                        DB.GetInstance.OpenConnection();
+                    DB.GetInstance.OpenConnection();
 
-                        for (int index = 0; index < dataGridView1.Rows.Count; index++)
+                    for (int index = 0; index < dataGridView1.Rows.Count; index++)
+                    {
+                        var rowState = (RowState)dataGridView1.Rows[index].Cells[4].Value;
+
+                        if (rowState == RowState.Deleted)
                         {
-                            var rowState = (RowState)dataGridView1.Rows[index].Cells[4].Value;
+                            var id = Convert.ToInt32(dataGridView1.Rows[index].Cells[0].Value);
+                            var login = dataGridView1.Rows[index].Cells[1].Value;
+                            var deleteQuery = $"delete from users where id = {id}";
 
-                            if (rowState == RowState.Deleted)
+                            var update_сharacteristics_вrigade = $"UPDATE сharacteristics_вrigade SET section_foreman_FIO = '' " +
+                                $"OR engineers_FIO = '' OR curator = '' OR departmentCommunications = '' " +
+                                $"WHERE section_foreman_FIO = '{login}' OR engineers_FIO = '{login}' OR curator = '{login}' " +
+                                $"OR departmentCommunications = '{login}'";
+
+                            using (MySqlCommand command = new MySqlCommand(deleteQuery, DB.GetInstance.GetConnection()))
                             {
-                                var id = Convert.ToInt32(dataGridView1.Rows[index].Cells[0].Value);
-                                var login = dataGridView1.Rows[index].Cells[1].Value;
-                                var deleteQuery = $"delete from users where id = {id}";
-
-                                var update_сharacteristics_вrigade = $"UPDATE сharacteristics_вrigade SET section_foreman_FIO = '' " +
-                                    $"OR engineers_FIO = '' OR curator = '' OR departmentCommunications = '' " +
-                                    $"WHERE section_foreman_FIO = '{login}' OR engineers_FIO = '{login}' OR curator = '{login}' " +
-                                    $"OR departmentCommunications = '{login}'";
-                                
-                                using (MySqlCommand command = new MySqlCommand(deleteQuery, DB.GetInstance.GetConnection()))
-                                {
-                                    command.ExecuteNonQuery();
-                                }
-                                using (MySqlCommand command2 = new MySqlCommand(update_сharacteristics_вrigade, DB.GetInstance.GetConnection()))
-                                {
-                                    command2.ExecuteNonQuery();
-                                }
-
+                                command.ExecuteNonQuery();
                             }
+                            using (MySqlCommand command2 = new MySqlCommand(update_сharacteristics_вrigade, DB.GetInstance.GetConnection()))
+                            {
+                                command2.ExecuteNonQuery();
+                            }
+
                         }
-                        DB.GetInstance.CloseConnection();
                     }
-                    RefreshDataGrid(dataGridView1);
+                    DB.GetInstance.CloseConnection();
                 }
-                catch (Exception)
-                {
-                    MessageBox.Show("Ошибка! Не возможно удалить данные (Button_delete_Click)");
-                }
+                RefreshDataGrid(dataGridView1);
             }
         }
         void Button_change_Click(object sender, EventArgs e)
         {
             if (Internet_check.CheackSkyNET())
             {
-                try
-                {
-                    var id = txB_id.Text;
-                    var login = txB_login.Text;
-                    var pass = Md5.EncryptPlainTextToCipherText(txB_pass.Text);
-                    var is_admin = cmB_is_admin_post.Text;
+                var id = txB_id.Text;
+                var login = txB_login.Text;
+                var pass = Md5.EncryptPlainTextToCipherText(txB_pass.Text);
+                var is_admin = cmB_is_admin_post.Text;
 
-                    var changeQuery = $"update users set login = '{login.Trim()}', pass = '{pass.Trim()}', is_Admin = '{is_admin.Trim()}' where id = '{id.Trim()}'";
+                var changeQuery = $"update users set login = '{login.Trim()}', pass = '{pass.Trim()}', is_Admin = '{is_admin.Trim()}' where id = '{id.Trim()}'";
 
-                    using (MySqlCommand command = new MySqlCommand(changeQuery, DB.GetInstance.GetConnection()))
-                    {
-                        DB.GetInstance.OpenConnection();
-                        command.ExecuteNonQuery();
-                        DB.GetInstance.CloseConnection();
-                        MessageBox.Show("Запись успешно изменена!");
-                    }
-                    RefreshDataGrid(dataGridView1);
-                }
-                catch (Exception)
+                using (MySqlCommand command = new MySqlCommand(changeQuery, DB.GetInstance.GetConnection()))
                 {
-                    MessageBox.Show("Ошибка! Не возможно изменить данные (Button_change_Click)");
+                    DB.GetInstance.OpenConnection();
+                    command.ExecuteNonQuery();
+                    DB.GetInstance.CloseConnection();
+                    MessageBox.Show("Запись успешно изменена!");
                 }
+                RefreshDataGrid(dataGridView1);
             }
         }
 
@@ -250,66 +201,59 @@ namespace ServiceTelecomConnect
 
         void Btn_add_Click(object sender, EventArgs e)
         {
-            try
+            if (Internet_check.CheackSkyNET())
             {
-                if (Internet_check.CheackSkyNET())
+                var loginUser = txB_login.Text;
+                if (!loginUser.Contains("-"))
                 {
-                    var loginUser = txB_login.Text;
-                    if (!loginUser.Contains("-"))
+                    if (!Regex.IsMatch(loginUser, @"^[А-ЯЁ][а-яё]*(([\s]+[А-Я][\.]+[А-Я]+[\.])$)"))
                     {
-                        if (!Regex.IsMatch(loginUser, @"^[А-ЯЁ][а-яё]*(([\s]+[А-Я][\.]+[А-Я]+[\.])$)"))
-                        {
-                            MessageBox.Show("Введите корректно поле \"Логин\"\nP.s. пример: Иванов В.В.", "Отмена", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                            txB_login.Select();
-                            return;
-                        }
-                    }
-                    if (loginUser.Contains("-"))
-                    {
-                        if (!Regex.IsMatch(loginUser, @"^[А-ЯЁ][а-яё]*(([\-][А-Я][а-яё]*[\s]+[А-Я]+[\.]+[А-Я]+[\.])$)"))
-                        {
-                            MessageBox.Show("Введите корректно поле \"Логин\"\nP.s. пример: Иванов-Петров В.В.", "Отмена", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                            txB_login.Select();
-                            return;
-                        }
-                    }
-
-
-                    var passUser = Md5.EncryptPlainTextToCipherText(txB_pass.Text);
-                    if (!CheackUser(loginUser, passUser))
-                    {
-                        if (!String.IsNullOrEmpty(cmB_is_admin_post.Text))
-                        {
-
-                            string querystring = $"INSERT INTO users (login, pass, is_admin) VALUES ('{loginUser}', '{passUser}', '{cmB_is_admin_post.Text}')";
-
-                            using (MySqlCommand command = new MySqlCommand(querystring, DB.GetInstance.GetConnection()))
-                            {
-                                DB.GetInstance.OpenConnection();
-
-                                if (command.ExecuteNonQuery() == 1)
-                                {
-                                    MessageBox.Show("Аккаунт успешно создан!");
-                                    RefreshDataGrid(dataGridView1);
-                                }
-                                else
-                                {
-                                    MessageBox.Show("Аккаунт не создан! Ошибка соединения");
-                                }
-                                DB.GetInstance.CloseConnection();
-                            }
-                        }
-                        else { MessageBox.Show("Должность и Дорога не должны быть пустыми!"); }
-                    }
-                    else
-                    {
-                        MessageBox.Show("Такой пользователь уже существует!");
+                        MessageBox.Show("Введите корректно поле \"Логин\"\nP.s. пример: Иванов В.В.", "Отмена", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        txB_login.Select();
+                        return;
                     }
                 }
-            }
-            catch (Exception)
-            {
-                MessageBox.Show("Ошибка регистрации!(Btn_add_Click)");
+                if (loginUser.Contains("-"))
+                {
+                    if (!Regex.IsMatch(loginUser, @"^[А-ЯЁ][а-яё]*(([\-][А-Я][а-яё]*[\s]+[А-Я]+[\.]+[А-Я]+[\.])$)"))
+                    {
+                        MessageBox.Show("Введите корректно поле \"Логин\"\nP.s. пример: Иванов-Петров В.В.", "Отмена", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        txB_login.Select();
+                        return;
+                    }
+                }
+
+
+                var passUser = Md5.EncryptPlainTextToCipherText(txB_pass.Text);
+                if (!CheackUser(loginUser, passUser))
+                {
+                    if (!String.IsNullOrEmpty(cmB_is_admin_post.Text))
+                    {
+
+                        string querystring = $"INSERT INTO users (login, pass, is_admin) VALUES ('{loginUser}', '{passUser}', '{cmB_is_admin_post.Text}')";
+
+                        using (MySqlCommand command = new MySqlCommand(querystring, DB.GetInstance.GetConnection()))
+                        {
+                            DB.GetInstance.OpenConnection();
+
+                            if (command.ExecuteNonQuery() == 1)
+                            {
+                                MessageBox.Show("Аккаунт успешно создан!");
+                                RefreshDataGrid(dataGridView1);
+                            }
+                            else
+                            {
+                                MessageBox.Show("Аккаунт не создан! Ошибка соединения");
+                            }
+                            DB.GetInstance.CloseConnection();
+                        }
+                    }
+                    else { MessageBox.Show("Должность и Дорога не должны быть пустыми!"); }
+                }
+                else
+                {
+                    MessageBox.Show("Такой пользователь уже существует!");
+                }
             }
         }
 
