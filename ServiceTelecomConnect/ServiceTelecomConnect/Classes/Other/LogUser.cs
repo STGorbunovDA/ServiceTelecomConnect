e88258c59300eb7a2e1554f421ae8fcf6ba15fde
STGorbunovDA <bous07@mail.ru>
2022-12-07 02:14:07 +0800

    Creating a method for tracking user actions in the program

---
+++ ServiceTelecomConnect/ServiceTelecomConnect/Classes/Other/LogUser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ServiceTelecomConnect.Classes.Other
+{
+    class LogUser
+    {
+        internal static void LogMethodUserSaveFilePC(string user, string method)
+        {
+            try
+            {
+                DateTime today = DateTime.Now;
+
+                string fileNamePath = $@"C:\Documents_ServiceTelekom\Log\{user}\{user}-{today.ToString("dd.MM.yyyy")}.txt";
+
+                if (!File.Exists($@"C:\Documents_ServiceTelekom\Log\{user}\"))
+                {
+                    Directory.CreateDirectory($@"C:\Documents_ServiceTelekom\Log\{user}\");
+                }
+
+                using (StreamWriter sw = new StreamWriter(fileNamePath, true, Encoding.Unicode))
+                {
+                    sw.WriteLine($"{user}\t{method}\t{today}");
+                }
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Файл не сохранен!(Log)");
+            }
+        }
+    }
+}
