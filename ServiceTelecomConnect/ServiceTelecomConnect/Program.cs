7a4f74089673ffd5557dc6455c993a6712c2a8cd
STGorbunovDA <bous07@mail.ru>
2022-12-07 04:34:43 +0800

    Creating a Global Catch

--- ServiceTelecomConnect/ServiceTelecomConnect/Program.cs
+++ ServiceTelecomConnect/ServiceTelecomConnect/Program.cs
@@ -1,16 +1,26 @@
-﻿using System;
+﻿using ServiceTelecomConnect.Classes.Other;
+using System;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace ServiceTelecomConnect
 {
     internal static class Program
     {
-        [STAThread] 
+        [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            Application.ThreadException += new ThreadExceptionEventHandler(Exception_global);
             Application.Run(new LoginForm());
-        }  
+        }
+
+        static void Exception_global(object sender, ThreadExceptionEventArgs e)
+        {
+            LogUser.LogExceptionUserSaveFilePC(e.Exception.Message);
+            LogUser.LogExceptionUserSaveFilePC(e.Exception.ToString());
+            MessageBox.Show(e.Exception.Message);
+        }
     }
 }
