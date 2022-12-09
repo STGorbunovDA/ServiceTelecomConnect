6a4ff3aa823b0a77211e26e0b520141de7c67f8e
STGorbunovDA <bous07@mail.ru>
2022-12-09 11:18:49 +0800

    Create form employee tabulation

--- ServiceTelecomConnect/ServiceTelecomConnect/Forms/ReportCardForm.Designer.cs
+++ ServiceTelecomConnect/ServiceTelecomConnect/Forms/ReportCardForm.Designer.cs
@@ -28,21 +28,200 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txB_timeCount = new System.Windows.Forms.TextBox();
+            this.txB_dateTimeExit = new System.Windows.Forms.TextBox();
+            this.txB_dateTimeInput = new System.Windows.Forms.TextBox();
+            this.txB_user = new System.Windows.Forms.TextBox();
+            this.txB_id = new System.Windows.Forms.TextBox();
+            this.picB_Update = new System.Windows.Forms.PictureBox();
+            this.btn_save_excel = new System.Windows.Forms.Button();
+            this.cmB_dateTimeInput = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picB_Update)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle7.BackColor = System.Drawing.Color.White;
+            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle7;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.SkyBlue;
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle8.BackColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle8.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Bold);
+            dataGridViewCellStyle8.ForeColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle8.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle8.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle8;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle9.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle9.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle9.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle9.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle9.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle9.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle9.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle9;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 48);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.ShowCellToolTips = false;
+            this.dataGridView1.Size = new System.Drawing.Size(796, 505);
+            this.dataGridView1.TabIndex = 91;
+            this.dataGridView1.CellBeginEdit += new System.Windows.Forms.DataGridViewCellCancelEventHandler(this.DataGridView1_CellBeginEdit);
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellClick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.txB_timeCount);
+            this.panel1.Controls.Add(this.txB_dateTimeExit);
+            this.panel1.Controls.Add(this.txB_dateTimeInput);
+            this.panel1.Controls.Add(this.txB_user);
+            this.panel1.Controls.Add(this.txB_id);
+            this.panel1.Location = new System.Drawing.Point(12, 7);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(199, 35);
+            this.panel1.TabIndex = 92;
+            this.panel1.Visible = false;
+            // 
+            // txB_timeCount
+            // 
+            this.txB_timeCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txB_timeCount.Location = new System.Drawing.Point(147, 3);
+            this.txB_timeCount.MaxLength = 49;
+            this.txB_timeCount.Multiline = true;
+            this.txB_timeCount.Name = "txB_timeCount";
+            this.txB_timeCount.Size = new System.Drawing.Size(33, 28);
+            this.txB_timeCount.TabIndex = 101;
+            // 
+            // txB_dateTimeExit
+            // 
+            this.txB_dateTimeExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txB_dateTimeExit.Location = new System.Drawing.Point(108, 3);
+            this.txB_dateTimeExit.MaxLength = 49;
+            this.txB_dateTimeExit.Multiline = true;
+            this.txB_dateTimeExit.Name = "txB_dateTimeExit";
+            this.txB_dateTimeExit.Size = new System.Drawing.Size(33, 28);
+            this.txB_dateTimeExit.TabIndex = 100;
+            // 
+            // txB_dateTimeInput
+            // 
+            this.txB_dateTimeInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txB_dateTimeInput.Location = new System.Drawing.Point(72, 3);
+            this.txB_dateTimeInput.MaxLength = 49;
+            this.txB_dateTimeInput.Multiline = true;
+            this.txB_dateTimeInput.Name = "txB_dateTimeInput";
+            this.txB_dateTimeInput.Size = new System.Drawing.Size(30, 28);
+            this.txB_dateTimeInput.TabIndex = 99;
+            // 
+            // txB_user
+            // 
+            this.txB_user.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txB_user.Location = new System.Drawing.Point(36, 3);
+            this.txB_user.MaxLength = 49;
+            this.txB_user.Multiline = true;
+            this.txB_user.Name = "txB_user";
+            this.txB_user.Size = new System.Drawing.Size(30, 28);
+            this.txB_user.TabIndex = 98;
+            // 
+            // txB_id
+            // 
+            this.txB_id.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txB_id.Location = new System.Drawing.Point(3, 3);
+            this.txB_id.MaxLength = 49;
+            this.txB_id.Multiline = true;
+            this.txB_id.Name = "txB_id";
+            this.txB_id.Size = new System.Drawing.Size(27, 28);
+            this.txB_id.TabIndex = 97;
+            // 
+            // picB_Update
+            // 
+            this.picB_Update.BackColor = System.Drawing.Color.Transparent;
+            this.picB_Update.BackgroundImage = global::ServiceTelecomConnect.Properties.Resources.icons8_синхронизация_подключения_321;
+            this.picB_Update.Location = new System.Drawing.Point(775, 12);
+            this.picB_Update.Name = "picB_Update";
+            this.picB_Update.Size = new System.Drawing.Size(33, 30);
+            this.picB_Update.TabIndex = 99;
+            this.picB_Update.TabStop = false;
+            this.picB_Update.Click += new System.EventHandler(this.PicB_Update_Click);
+            // 
+            // btn_save_excel
+            // 
+            this.btn_save_excel.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_save_excel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_save_excel.Location = new System.Drawing.Point(618, 11);
+            this.btn_save_excel.Name = "btn_save_excel";
+            this.btn_save_excel.Size = new System.Drawing.Size(151, 31);
+            this.btn_save_excel.TabIndex = 100;
+            this.btn_save_excel.Text = "Сохранить в excel";
+            this.btn_save_excel.UseVisualStyleBackColor = false;
+            this.btn_save_excel.Click += new System.EventHandler(this.Btn_save_excel_Click);
+            // 
+            // cmB_dateTimeInput
+            // 
+            this.cmB_dateTimeInput.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmB_dateTimeInput.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmB_dateTimeInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cmB_dateTimeInput.FormattingEnabled = true;
+            this.cmB_dateTimeInput.Items.AddRange(new object[] {
+            "Модель",
+            "Неисправность",
+            "Автор",
+            "Описание неисправности"});
+            this.cmB_dateTimeInput.Location = new System.Drawing.Point(384, 10);
+            this.cmB_dateTimeInput.Name = "cmB_dateTimeInput";
+            this.cmB_dateTimeInput.Size = new System.Drawing.Size(228, 28);
+            this.cmB_dateTimeInput.TabIndex = 101;
+            this.cmB_dateTimeInput.SelectionChangeCommitted += new System.EventHandler(this.CmB_dateTimeInput_SelectionChangeCommitted);
             // 
             // ReportCardForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = global::ServiceTelecomConnect.Properties.Resources.Untitled_6;
+            this.ClientSize = new System.Drawing.Size(820, 565);
+            this.Controls.Add(this.cmB_dateTimeInput);
+            this.Controls.Add(this.btn_save_excel);
+            this.Controls.Add(this.picB_Update);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dataGridView1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximumSize = new System.Drawing.Size(836, 604);
+            this.MinimumSize = new System.Drawing.Size(836, 604);
             this.Name = "ReportCardForm";
-            this.Text = "ReportCardForm";
+            this.Text = "Табель";
             this.Load += new System.EventHandler(this.ReportCardForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picB_Update)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel1;
+        internal System.Windows.Forms.TextBox txB_id;
+        internal System.Windows.Forms.TextBox txB_user;
+        internal System.Windows.Forms.TextBox txB_dateTimeInput;
+        internal System.Windows.Forms.TextBox txB_dateTimeExit;
+        internal System.Windows.Forms.TextBox txB_timeCount;
+        private System.Windows.Forms.PictureBox picB_Update;
+        private System.Windows.Forms.Button btn_save_excel;
+        private System.Windows.Forms.ComboBox cmB_dateTimeInput;
     }
 }
--- ServiceTelecomConnect/ServiceTelecomConnect/Forms/ReportCardForm.cs
+++ ServiceTelecomConnect/ServiceTelecomConnect/Forms/ReportCardForm.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,15 +17,181 @@ namespace ServiceTelecomConnect.Forms
 {
     public partial class ReportCardForm : Form
     {
+        int selectedRow;
         public ReportCardForm()
         {
             StartPosition = FormStartPosition.CenterScreen;
             InitializeComponent();
         }
+        private void CreateColums()
+        {
+            dataGridView1.Columns.Add("id", "№");
+            dataGridView1.Columns.Add("user", "Работник");
+            dataGridView1.Columns.Add("dateTimeInput", "Дата входа");
+            dataGridView1.Columns.Add("dateTimeExit", "Дата выхода");
+            dataGridView1.Columns.Add("TimeCount", "Время нахождения");
+            dataGridView1.Columns[0].Visible = false;
+        }
+        void ReedSingleRow(DataGridView dgw, IDataRecord record)
+        {
+            dataGridView1.Invoke((MethodInvoker)(() => dgw.Rows.Add(record.GetInt32(0), record.GetString(1),
+                record.GetDateTime(2), record.GetDateTime(3), record.GetDateTime(3).Subtract(record.GetDateTime(2)), RowState.ModifieldNew)));
+        }
+
+        void RefreshDataGrid(DataGridView dgw)
+        {
+            if (Internet_check.CheackSkyNET())
+            {
+                var myCulture = new CultureInfo("ru-RU");
+                myCulture.NumberFormat.NumberDecimalSeparator = ".";
+                Thread.CurrentThread.CurrentCulture = myCulture;
+                dgw.Rows.Clear();
+
+                string queryString = $"SELECT id, user, dateTimeInput, dateTimeExit FROM logUserDB";
+
+                using (MySqlCommand command = new MySqlCommand(queryString, DB.GetInstance.GetConnection()))
+                {
+                    DB.GetInstance.OpenConnection();
+
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                ReedSingleRow(dgw, reader);
+                            }
+                            reader.Close();
+                        }
+                    }
+                    command.ExecuteNonQuery();
+                    DB.GetInstance.CloseConnection();
+                }
+                dataGridView1.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
+                dataGridView1.AutoResizeRows(DataGridViewAutoSizeRowsMode.AllCells);
+            }
+        }
 
         void ReportCardForm_Load(object sender, EventArgs e)
         {
+            CreateColums();
+            RefreshDataGrid(dataGridView1);
 
+            string querystring2 = $"SELECT DISTINCT DATE(dateTimeInput) FROM logUserDB ORDER BY dateTimeInput";
+            using (MySqlCommand command = new MySqlCommand(querystring2, DB.GetInstance.GetConnection()))
+            {
+                DB.GetInstance.OpenConnection();
+                DataTable table = new DataTable();
+
+                using (MySqlDataAdapter adapter = new MySqlDataAdapter(command))
+                {
+                    adapter.Fill(table);
+
+                    cmB_dateTimeInput.DataSource = table;
+                    cmB_dateTimeInput.DisplayMember = "DATE(dateTimeInput)";
+                    cmB_dateTimeInput.ValueMember = "DATE(dateTimeInput)";
+                    DB.GetInstance.CloseConnection();
+                }
+            }
+        }
+
+        void DataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            dataGridView1.ReadOnly = false;
+
+            selectedRow = e.RowIndex;
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[selectedRow];
+                txB_id.Text = row.Cells[0].Value.ToString();
+                txB_user.Text = row.Cells[1].Value.ToString();
+                txB_dateTimeInput.Text = row.Cells[2].Value.ToString();
+                txB_dateTimeExit.Text = row.Cells[3].Value.ToString();
+                txB_timeCount.Text = row.Cells[4].Value.ToString();
+            }
+        }
+
+        void DataGridView1_CellBeginEdit(object sender, DataGridViewCellCancelEventArgs e)
+        {
+            if (e.ColumnIndex != 0)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        void PicB_Update_Click(object sender, EventArgs e)
+        {
+            RefreshDataGrid(dataGridView1);
+        }
+
+        void Btn_save_excel_Click(object sender, EventArgs e)
+        {
+            DateTime dateTime = DateTime.Now;
+            string dateTimeString = dateTime.ToString("dd.MM.yyyy");
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "csv files (*.csv)|*.csv|All files (*.*)|*.*";
+            sfd.FileName = $"Табель сотрудников_{dateTimeString}";
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.Unicode))
+                {
+                    string note = string.Empty;
+
+                    note += $"Работник\tДата входа\tДата выхода\tВремя нахождения";
+
+                    sw.WriteLine(note);
+
+                    for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                    {
+                        for (int j = 0; j < dataGridView1.ColumnCount; j++)
+                        {
+                            var re = new Regex(Environment.NewLine);
+                            var value = dataGridView1.Rows[i].Cells[j].Value.ToString();
+                            value = re.Replace(value, " ");
+                            if (dataGridView1.Columns[j].HeaderText.ToString() == "№")
+                            {
+
+                            }
+                            else if (dataGridView1.Columns[j].HeaderText.ToString() == "Время нахождения")
+                            {
+                                sw.Write(value);
+                            }
+                            else sw.Write(value + "\t");
+                        }
+                        sw.WriteLine();
+                    }
+
+                }
+                MessageBox.Show("Файл успешно сохранен!");
+            }
+        }
+
+        void CmB_dateTimeInput_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            if (cmB_dateTimeInput.Items.Count == 0)
+            {
+                return;
+            }
+
+            var searchString = $"SELECT id, user, dateTimeInput, dateTimeExit FROM logUserDB WHERE CONCAT (dateTimeInput) LIKE '%" + cmB_dateTimeInput.Text + "%'";
+            using (MySqlCommand command = new MySqlCommand(searchString, DB.GetInstance.GetConnection()))
+            {
+                DB.GetInstance.OpenConnection();
+
+                using (MySqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            ReedSingleRow(dataGridView1, reader);
+                        }
+                        reader.Close();
+                    }
+                }
+                DB.GetInstance.CloseConnection();
+            }
         }
     }
 }
