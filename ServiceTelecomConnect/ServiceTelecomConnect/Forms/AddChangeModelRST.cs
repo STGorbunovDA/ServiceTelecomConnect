bbbe21e2e712c8e73f6b4e33f02d8e7e6bfd4be8
STGorbunovDA <bous07@mail.ru>
2022-12-09 12:28:23 +0800

    Refactoring code

--- ServiceTelecomConnect/ServiceTelecomConnect/Forms/AddChangeModelRST.cs
+++ ServiceTelecomConnect/ServiceTelecomConnect/Forms/AddChangeModelRST.cs
@@ -1,43 +1,26 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ServiceTelecomConnect.Forms
 {
     public partial class AddChangeModelRST : Form
     {
+        string selectedItem_cmB_model = String.Empty;
+
         public AddChangeModelRST()
         {
             InitializeComponent();
             StartPosition = FormStartPosition.CenterScreen;
         }
+        private void CmB_model_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            selectedItem_cmB_model = cmB_model.GetItemText(cmB_model.SelectedItem);
+        }
 
         void AddChangeModelRST_Load(object sender, EventArgs e)
         {
-            if (Internet_check.CheackSkyNET())
-            {
-                DB.GetInstance.OpenConnection();
-                string querystring = $"SELECT id, model_radiostation_name FROM model_radiostation";
-                using (MySqlCommand command = new MySqlCommand(querystring, DB.GetInstance.GetConnection()))
-                {
-                    DataTable model_RSR_table = new DataTable();
-                    using (MySqlDataAdapter adapter = new MySqlDataAdapter(command))
-                    {
-                        adapter.Fill(model_RSR_table);
-                        cmB_model.DataSource = model_RSR_table;
-                        cmB_model.ValueMember = "id";
-                        cmB_model.DisplayMember = "model_radiostation_name";
-                    }
-                }
-                DB.GetInstance.CloseConnection();
-            }
+            QuerySettingDataBase.GettingModelRST_CMB(cmB_model);
         }
 
         void Btn_add_modelRST_Click(object sender, EventArgs e)
@@ -52,32 +35,47 @@ namespace ServiceTelecomConnect.Forms
                     command.ExecuteNonQuery();
                     MessageBox.Show("Модель радиостанции успешно добавлена!");
                     DB.GetInstance.CloseConnection();
-                }    
-
-                string querystring = $"SELECT id, model_radiostation_name FROM model_radiostation";
-                using (MySqlCommand command2 = new MySqlCommand(querystring, DB.GetInstance.GetConnection()))
-                {
-                    DB.GetInstance.OpenConnection();
-                    DataTable table = new DataTable();
-
-                    using (MySqlDataAdapter adapter = new MySqlDataAdapter(command2))
-                    {
-                        adapter.Fill(table);
-                        if (table.Rows.Count > 0)
-                        {
-                            cmB_model.DataSource = table;
-                            cmB_model.ValueMember = "id";
-                            cmB_model.DisplayMember = "model_radiostation_name";
-                        }
-                        DB.GetInstance.CloseConnection();
-                    }
                 }
+
+                QuerySettingDataBase.GettingModelRST_CMB(cmB_model);
             }
         }
 
         void Btn_change_modelRST_Click(object sender, EventArgs e)
         {
+            if (Internet_check.CheackSkyNET())
+            {
+                var addQuery = $"UPDATE model_radiostation SET model_radiostation_name = '{cmB_model.Text}' WHERE model_radiostation_name = '{selectedItem_cmB_model}'";
 
+                using (MySqlCommand command = new MySqlCommand(addQuery, DB.GetInstance.GetConnection()))
+                {
+                    DB.GetInstance.OpenConnection();
+                    command.ExecuteNonQuery();
+                    QuerySettingDataBase.GettingModelRST_CMB(cmB_model);
+                    MessageBox.Show("Модель радиостанции успешно изменена!");
+                    DB.GetInstance.CloseConnection();
+                }
+            }
+        }
+
+        void Btn_delete_modelRST_Click(object sender, EventArgs e)
+        {
+            string Mesage;
+            Mesage = $"Вы действительно хотите удалить модель радиостанции?\n Модель: {cmB_model.GetItemText(cmB_model.SelectedItem)}";
+
+            if (MessageBox.Show(Mesage, "Внимание", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == DialogResult.No)
+            {
+                return;
+            }
+            var deleteQuery = $"delete from model_radiostation where model_radiostation_name = '{cmB_model.GetItemText(cmB_model.SelectedItem)}'";
+            using (MySqlCommand command = new MySqlCommand(deleteQuery, DB.GetInstance.GetConnection()))
+            {
+                DB.GetInstance.OpenConnection();
+                command.ExecuteNonQuery();
+                QuerySettingDataBase.GettingModelRST_CMB(cmB_model);
+                MessageBox.Show("Модель радиостанции успешно удалена!");
+                DB.GetInstance.CloseConnection();
+            }
         }
     }
 }
