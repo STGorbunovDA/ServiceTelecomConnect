17bf4a90af2fc152e407dd312f2024fa42dbd554
STGorbunovDA <bous07@mail.ru>
2023-01-07 23:56:39 +0800

    Refactoring code

--- ServiceTelecomConnect/ServiceTelecomConnect/Classes/Important/FunctionPanel.cs
+++ ServiceTelecomConnect/ServiceTelecomConnect/Classes/Important/FunctionPanel.cs
@@ -824,7 +824,6 @@ namespace ServiceTelecomConnect
                 if (!String.IsNullOrEmpty(openFile.FileName))
                 {
                     string filename = openFile.FileName;
-                    string text = File.ReadAllText(filename);
 
                     var lineNumber = 0;
 
@@ -836,23 +835,14 @@ namespace ServiceTelecomConnect
 
                             if (lineNumber != 0)
                             {
-                                var values = line.Split('\t');
-
-                                if (!CheacSerialNumber.GetInstance.CheacSerialNumber_radiostantion_last_year(values[4]))
+                                var values = line.Split(';');
+                                var dateTO = Convert.ToDateTime(values[7]).ToString("yyyy-MM-dd");
+                                if (!CheacSerialNumber.GetInstance.CheacSerialNumber_radiostantion_full(values[4]))
                                 {
                                     var mySql = $"INSERT INTO radiostantion_full (poligon, company, location, model, serialNumber," +
-                                    $"inventoryNumber, networkNumber, dateTO, numberAct, city, price, representative, " +
-                                    $"post, numberIdentification, dateIssue, phoneNumber, numberActRemont, category, priceRemont, " +
-                                    $"antenna, manipulator, AKB, batteryСharger, completed_works_1, completed_works_2, completed_works_3, " +
-                                    $"completed_works_4, completed_works_5, completed_works_6, completed_works_7, parts_1, parts_2, parts_3, parts_4, " +
-                                    $"parts_5, parts_6, parts_7, decommissionSerialNumber, comment, road) VALUES " +
-                                    $"('{values[0]}', '{values[1]}', '{values[2]}', '{values[3]}','{values[4]}', '{values[5]}', '{values[6]}', " +
-                                    $"'{values[7]}','{values[8]}','{values[9]}','{values[10]}', '{values[11]}', '{values[12]}', " +
-                                    $"'{values[13]}', '{values[14]}', '{values[15]}', '{values[16]}', '{values[17]}', '{values[18]}'," +
-                                    $"'{values[19]}', '{values[20]}', '{values[21]}', '{values[22]}', '{values[23]}', '{values[24]}', " +
-                                    $"'{values[25]}', '{values[26]}', '{values[27]}', '{values[28]}', '{values[29]}', '{values[30]}', " +
-                                    $"'{values[31]}', '{values[32]}', '{values[33]}', '{values[34]}', '{values[35]}', '{values[36]}', " +
-                                    $"'{values[37]}', '{values[38]}', '{values[39]}')";
+                                    $"inventoryNumber, networkNumber, dateTO, numberAct, city, price, road) VALUES " +
+                                    $"('{values[0].Trim()}', '{values[1].Trim()}', '{values[2].Trim()}', '{values[3].Trim()}','{values[4].Trim()}', '{values[5].Trim()}', '{values[6].Trim()}', " +
+                                    $"'{dateTO.Trim()}','{values[8].Trim()}','{values[9].Trim()}','{values[10].Trim()}', '{values[11].Trim()}')";
 
                                     using (MySqlCommand command = new MySqlCommand(mySql, DB.GetInstance.GetConnection()))
                                     {
