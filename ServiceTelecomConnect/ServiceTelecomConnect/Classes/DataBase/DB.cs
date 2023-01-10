55e2d5b49e0e6f3c7b19a722b03982776ac266cd
STGorbunovDA <bous07@mail.ru>
2023-01-11 05:06:55 +0800

    Refactoring code

--- ServiceTelecomConnect/ServiceTelecomConnect/Classes/DataBase/DB.cs
+++ ServiceTelecomConnect/ServiceTelecomConnect/Classes/DataBase/DB.cs
@@ -33,18 +33,13 @@ namespace ServiceTelecomConnect
         public void OpenConnection()
         {
             if (connection.State == System.Data.ConnectionState.Closed)
-            {
                 connection.Open();
-            }
-
         }
 
         public void CloseConnection()
         {
             if (connection.State == System.Data.ConnectionState.Open)
-            {
                 connection.Close();
-            }
         }
     }
 }
--- ServiceTelecomConnect/ServiceTelecomConnect/Classes/DataBase/DB_2.cs
+++ ServiceTelecomConnect/ServiceTelecomConnect/Classes/DataBase/DB_2.cs
@@ -33,17 +33,14 @@ namespace ServiceTelecomConnect
         public void OpenConnection()
         {
             if (connection.State == System.Data.ConnectionState.Closed)
-            {
                 connection.Open();
-            }
+
         }
 
         public void CloseConnection()
         {
             if (connection.State == System.Data.ConnectionState.Open)
-            {
                 connection.Close();
-            }
         }
     }
 }
