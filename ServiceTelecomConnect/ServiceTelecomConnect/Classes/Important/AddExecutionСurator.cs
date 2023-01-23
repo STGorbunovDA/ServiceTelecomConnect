94e03dca106a841c52534d7aea68c402a2320ae5
STGorbunovDA <bous07@mail.ru>
2023-01-23 22:52:51 +0800

    Refactoring code

--- ServiceTelecomConnect/ServiceTelecomConnect/Classes/Important/AddExecutionСurator.cs
+++ ServiceTelecomConnect/ServiceTelecomConnect/Classes/Important/AddExecutionСurator.cs
@@ -12,34 +12,34 @@ namespace ServiceTelecomConnect
         {
             if (Internet_check.CheackSkyNET())
             {
-                var AddExecutionQuery = String.Empty;
+                string AddExecutionQuery = String.Empty;
                 foreach (DataGridViewRow row in dgw.SelectedRows)
                     dgw.Rows[row.Index].Cells[41].Value = months;
 
                 for (int index = 0; index < dgw.Rows.Count; index++)
                 {
-                    var rowState = dgw.Rows[index].Cells[41].Value.ToString();//проверить индекс
+                    string rowState = dgw.Rows[index].Cells[41].Value.ToString();//проверить индекс
 
                     if (rowState == months)
                     {
-                        var poligon = dgw.Rows[index].Cells[1].Value.ToString();
-                        var company = dgw.Rows[index].Cells[2].Value.ToString();
-                        var location = dgw.Rows[index].Cells[3].Value.ToString();
-                        var model = dgw.Rows[index].Cells[4].Value.ToString();
-                        var serialNumber = dgw.Rows[index].Cells[5].Value.ToString();
-                        var inventoryNumber = dgw.Rows[index].Cells[6].Value.ToString();
-                        var networkNumber = dgw.Rows[index].Cells[7].Value.ToString();
+                        string poligon = dgw.Rows[index].Cells[1].Value.ToString();
+                        string company = dgw.Rows[index].Cells[2].Value.ToString();
+                        string location = dgw.Rows[index].Cells[3].Value.ToString();
+                        string model = dgw.Rows[index].Cells[4].Value.ToString();
+                        string serialNumber = dgw.Rows[index].Cells[5].Value.ToString();
+                        string inventoryNumber = dgw.Rows[index].Cells[6].Value.ToString();
+                        string networkNumber = dgw.Rows[index].Cells[7].Value.ToString();
                         DateTime _dateTO = (DateTime)dgw.Rows[index].Cells[8].Value;
-                        var dateTO = Convert.ToDateTime(_dateTO).ToString("yyyy-MM-dd");
-                        var numberAct = dgw.Rows[index].Cells[9].Value.ToString();
-                        var city = dgw.Rows[index].Cells[10].Value.ToString();
-                        var price = dgw.Rows[index].Cells[11].Value.ToString();
-                        var numberActRemont = dgw.Rows[index].Cells[17].Value.ToString();
-                        var category = dgw.Rows[index].Cells[18].Value.ToString();
-                        var priceRemont = dgw.Rows[index].Cells[19].Value.ToString();
-                        var decommissionSerialNumber = dgw.Rows[index].Cells[38].Value.ToString();
-                        var comment = dgw.Rows[index].Cells[39].Value.ToString();
-                        var road = dgw.Rows[index].Cells[40].Value.ToString();
+                        string dateTO = Convert.ToDateTime(_dateTO).ToString("yyyy-MM-dd");
+                        string numberAct = dgw.Rows[index].Cells[9].Value.ToString();
+                        string city = dgw.Rows[index].Cells[10].Value.ToString();
+                        string price = dgw.Rows[index].Cells[11].Value.ToString();
+                        string numberActRemont = dgw.Rows[index].Cells[17].Value.ToString();
+                        string category = dgw.Rows[index].Cells[18].Value.ToString();
+                        string priceRemont = dgw.Rows[index].Cells[19].Value.ToString();
+                        string decommissionSerialNumber = dgw.Rows[index].Cells[38].Value.ToString();
+                        string comment = dgw.Rows[index].Cells[39].Value.ToString();
+                        string road = dgw.Rows[index].Cells[40].Value.ToString();
 
                         if (!CheacSerialNumber.GetInstance.CheacSerialNumber_radiostantionCurator(road, city, serialNumber))
                         {
@@ -82,15 +82,15 @@ namespace ServiceTelecomConnect
 
                 for (int index = 0; index < dgw.Rows.Count; index++)
                 {
-                    var rowState = dgw.Rows[index].Cells[19].Value.ToString();//проверить индекс
+                    string rowState = dgw.Rows[index].Cells[19].Value.ToString();//проверить индекс
 
                     if (rowState == month)
                     {
-                        var company = dgw.Rows[index].Cells[2].Value.ToString();
-                        var serialNumber = dgw.Rows[index].Cells[5].Value.ToString();
-                        var inventoryNumber = dgw.Rows[index].Cells[6].Value.ToString();
-                        var networkNumber = dgw.Rows[index].Cells[7].Value.ToString();
-                        var decommissionSerialNumber = dgw.Rows[index].Cells[15].Value.ToString();
+                        string company = dgw.Rows[index].Cells[2].Value.ToString();
+                        string serialNumber = dgw.Rows[index].Cells[5].Value.ToString();
+                        string inventoryNumber = dgw.Rows[index].Cells[6].Value.ToString();
+                        string networkNumber = dgw.Rows[index].Cells[7].Value.ToString();
+                        string decommissionSerialNumber = dgw.Rows[index].Cells[15].Value.ToString();
 
                         if (inventoryNumber == "списание" || networkNumber == "списание" || !string.IsNullOrEmpty(decommissionSerialNumber)
                             || string.IsNullOrEmpty(inventoryNumber) || string.IsNullOrEmpty(networkNumber) || inventoryNumber == "НЕТ"
@@ -102,7 +102,7 @@ namespace ServiceTelecomConnect
                                 continue;
                         }
 
-                        var AddExecutionQuery = $"UPDATE radiostantion_сomparison SET month = '{month}' WHERE serialNumber = '{serialNumber}' AND road = '{road.Text}'";
+                        string AddExecutionQuery = $"UPDATE radiostantion_сomparison SET month = '{month}' WHERE serialNumber = '{serialNumber}' AND road = '{road.Text}'";
 
                         using (MySqlCommand command = new MySqlCommand(AddExecutionQuery, DB_4.GetInstance.GetConnection()))
                         {
