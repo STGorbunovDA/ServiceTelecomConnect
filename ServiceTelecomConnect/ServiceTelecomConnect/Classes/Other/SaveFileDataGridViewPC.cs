3a4caf6d6ed3d3e70b0272ca3340835e1bcc216f
STGorbunovDA <bous07@mail.ru>
2023-01-29 16:05:29 +0800

    Refactoring code

--- ServiceTelecomConnect/ServiceTelecomConnect/Classes/Other/SaveFileDataGridViewPC.cs
+++ ServiceTelecomConnect/ServiceTelecomConnect/Classes/Other/SaveFileDataGridViewPC.cs
@@ -10,7 +10,6 @@ namespace ServiceTelecomConnect
     {
         #region Сохранение БД на PC пользователем
 
-
         internal static void DirectorateSaveFilePC(DataGridView dgw, string cmB_city)
         {
             DateTime dateTime = DateTime.Now;
@@ -358,7 +357,7 @@ namespace ServiceTelecomConnect
                         $"Номер Акта ремонта\tКатегория\tЦена ремонта\tАнтенна\tМанипулятор\tАКБ\tЗУ\tВыполненные работы_1\t" +
                         $"Выполненные работы_2\tВыполненные работы_3\tВыполненные работы_4\tВыполненные работы_5\t" +
                         $"Выполненные работы_6\tВыполненные работы_7\tДеталь_1\tДеталь_2\tДеталь_3\tДеталь_4\tДеталь_5\t" +
-                        $"Деталь_6\tДеталь_7\t№ Акта списания\tПримечания\tДорога";
+                        $"Деталь_6\tДеталь_7\t№ Акта списания\tПримечания\tДорога\tСостояние РСТ";
 
                     sw.WriteLine(note);
 
@@ -375,7 +374,7 @@ namespace ServiceTelecomConnect
                             }
                             else if (dgw.Columns[j].HeaderText.ToString() == "Дата ТО")
                                 sw.Write(Convert.ToDateTime(value).ToString("dd.MM.yyyy") + "\t");
-                            else if (dgw.Columns[j].HeaderText.ToString() == "Дорога")
+                            else if (dgw.Columns[j].HeaderText.ToString() == "Состояние РСТ")
                                 sw.Write(value);
                             else if (dgw.Columns[j].HeaderText.ToString() == "RowState")
                             {
@@ -417,7 +416,7 @@ namespace ServiceTelecomConnect
                     $"Номер Акта ремонта\tКатегория\tЦена ремонта\tАнтенна\tМанипулятор\tАКБ\tЗУ\tВыполненные работы_1\t" +
                     $"Выполненные работы_2\tВыполненные работы_3\tВыполненные работы_4\tВыполненные работы_5\t" +
                     $"Выполненные работы_6\tВыполненные работы_7\tДеталь_1\tДеталь_2\tДеталь_3\tДеталь_4\tДеталь_5\t" +
-                    $"Деталь_6\tДеталь_7\t№ Акта списания\tПримечания\tДорога";
+                    $"Деталь_6\tДеталь_7\t№ Акта списания\tПримечания\tДорога\tСостояние РСТ";
 
                 sw.WriteLine(note);
 
@@ -434,7 +433,7 @@ namespace ServiceTelecomConnect
                         }
                         else if (dgw.Columns[j].HeaderText.ToString() == "Дата ТО")
                             sw.Write(Convert.ToDateTime(value).ToString("dd.MM.yyyy") + "\t");
-                        else if (dgw.Columns[j].HeaderText.ToString() == "Дорога")
+                        else if (dgw.Columns[j].HeaderText.ToString() == "Состояние РСТ")
                             sw.Write(value);
                         else if (dgw.Columns[j].HeaderText.ToString() == "RowState")
                         {
