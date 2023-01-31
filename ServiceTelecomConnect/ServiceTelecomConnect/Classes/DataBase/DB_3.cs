19998e41316abd923799e4bebe25c8aeec6ef236
STGorbunovDA <bous07@mail.ru>
2023-01-31 11:59:04 +0800

    Refactoring code

--- ServiceTelecomConnect/ServiceTelecomConnect/Classes/DataBase/DB_3.cs
+++ ServiceTelecomConnect/ServiceTelecomConnect/Classes/DataBase/DB_3.cs
@@ -2,9 +2,8 @@
 
 namespace ServiceTelecomConnect
 {
-    class DB_3
+    class DB_3 //для копирования radiostantion в radiostantion_copy в отдельном потоке по таймеру
     {
-
         static volatile DB_3 Class;
         static object SyncObject = new object();
         public static DB_3 GetInstance
@@ -21,11 +20,10 @@ namespace ServiceTelecomConnect
             }
         }
 
-        /// "server=31.31.198.62;port=3306;username=u1748936_db_3;password=war74_89;database=u1748936_root;charset=utf8" /// для просмотра
-        /// "server=31.31.198.62;port=3306;username=u1748936_db_3_2;password=war74_89;database=u1748936_radiostantion;charset=utf8" /// для работы
+        /// "server=31.31.198.62;port=3306;username=u1748936_db_2;password=war74_89;database=u1748936_root;charset=utf8" /// для просмотра
+        /// "server=31.31.198.62;port=3306;username=u1748936_db_2_1;password=war74_89;database=u1748936_radiostantion;charset=utf8" /// для работы
 
-
-        readonly MySqlConnection connection = new MySqlConnection("server=31.31.198.62;port=3306;username=u1748936_db_3_2;password=war74_90;database=u1748936_radiostantion;charset=utf8");
+        readonly MySqlConnection connection = new MySqlConnection("server=31.31.198.62;port=3306;username=u1748936_engener;password=55557777Engener;database=u1748936_radiostantion;charset=utf8");
 
         public MySqlConnection GetConnection()
         {
@@ -35,17 +33,14 @@ namespace ServiceTelecomConnect
         public void OpenConnection()
         {
             if (connection.State == System.Data.ConnectionState.Closed)
-            {
                 connection.Open();
-            }
+
         }
 
         public void CloseConnection()
         {
             if (connection.State == System.Data.ConnectionState.Open)
-            {
                 connection.Close();
-            }
         }
     }
 }
