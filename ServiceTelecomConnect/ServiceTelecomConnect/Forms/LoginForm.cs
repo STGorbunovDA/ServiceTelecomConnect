491b875520303d098dfdc57dff1058e2089ee6fc
STGorbunovDA <bous07@mail.ru>
2023-02-02 11:14:31 +0800

    Refactoring code

--- ServiceTelecomConnect/ServiceTelecomConnect/Forms/LoginForm.cs
+++ ServiceTelecomConnect/ServiceTelecomConnect/Forms/LoginForm.cs
@@ -3,7 +3,6 @@ using MySql.Data.MySqlClient;
 using System;
 using System.Data;
 using System.Drawing;
-using System.IO;
 using System.Linq;
 using System.Windows.Forms;
 
@@ -16,7 +15,6 @@ namespace ServiceTelecomConnect
         {
             InitializeComponent();
             if (!InstanceChecker.TakeMemory())
-
                 StartPosition = FormStartPosition.CenterScreen;
             Environment.GetCommandLineArgs().ToList().ForEach(x =>
             {
@@ -27,14 +25,14 @@ namespace ServiceTelecomConnect
                 }
             });
         }
-        void LoginForm_Load(object sender, EventArgs e)
+        void LoginFormLoad(object sender, EventArgs e)
         {
             txB_passField.PasswordChar = '*';
             lbL_hidePassword.Visible = false;
             txB_loginField.MaxLength = 100;
             txB_passField.MaxLength = 32;
             if (txB_loginField.Text == "Admin" || txB_passField.Text == "1818")
-                EnterButtonLogin_Click(sender, e);
+                EnterButtonLoginClick(sender, e);
             try
             {
                 RegistryKey reg1 = Registry.CurrentUser.OpenSubKey($"SOFTWARE\\ServiceTelekom_Setting\\Login_Password");
@@ -51,24 +49,21 @@ namespace ServiceTelecomConnect
             {
             }
         }
-        void EnterButtonLogin_Click(object sender, EventArgs e)
+        void EnterButtonLoginClick(object sender, EventArgs e)
         {
             if (Internet_check.CheackSkyNET())
             {
                 string loginUser = txB_loginField.Text;
                 string passUser = Md5.EncryptPlainTextToCipherText(txB_passField.Text);
-
-                string querystring = $"SELECT id, login, pass, is_admin	FROM users WHERE login = '{loginUser}' AND pass = '{passUser}'";
-
+                string querystring = $"SELECT id, login, pass, is_admin	FROM users " +
+                    $"WHERE login = '{loginUser}' AND pass = '{passUser}'";
                 using (MySqlCommand command = new MySqlCommand(querystring, DB.GetInstance.GetConnection()))
                 {
                     DB.GetInstance.OpenConnection();
                     using (MySqlDataAdapter adapter = new MySqlDataAdapter(command))
                     {
                         DataTable table = new DataTable();
-
                         adapter.Fill(table);
-
                         if (table.Rows.Count == 1)
                         {
                             CheakUser user = new CheakUser(table.Rows[0].ItemArray[1].ToString(), table.Rows[0].ItemArray[3].ToString());
@@ -93,25 +88,24 @@ namespace ServiceTelecomConnect
                 }
             }
         }
-        void OpenPassword_Click(object sender, EventArgs e)
+        void OpenPasswordClick(object sender, EventArgs e)
         {
             txB_passField.UseSystemPasswordChar = true;
             lbL_hidePassword.Visible = true;
             lbL_openPassword.Visible = false;
         }
-        void HidePassword_Click(object sender, EventArgs e)
+        void HidePasswordClick(object sender, EventArgs e)
         {
             txB_passField.UseSystemPasswordChar = false;
             lbL_hidePassword.Visible = false;
             lbL_openPassword.Visible = true;
         }
-        void Clear_Click(object sender, EventArgs e)
+        void ClearClick(object sender, EventArgs e)
         {
-            txB_loginField.Text = "";
-            txB_passField.Text = "";
+            txB_loginField.Text = String.Empty;
+            txB_passField.Text = String.Empty;
         }
-
-        void RegistrationLoginForm_Click(object sender, EventArgs e)
+        void RegistrationLoginFormClick(object sender, EventArgs e)
         {
             using (RegistrationForm registrationForm = new RegistrationForm())
             {
@@ -122,47 +116,38 @@ namespace ServiceTelecomConnect
         }
 
         #region Подсветка
-
-        void RegistrationLoginForm_MouseEnter(object sender, EventArgs e)
+        void RegistrationLoginFormMouseEnter(object sender, EventArgs e)
         {
             lbL_registrationLoginForm.ForeColor = Color.White;
         }
-
-        void RegistrationLoginForm_MouseLeave(object sender, EventArgs e)
+        void RegistrationLoginFormMouseLeave(object sender, EventArgs e)
         {
             lbL_registrationLoginForm.ForeColor = Color.Black;
         }
-
-        void OpenPassword_MouseEnter(object sender, EventArgs e)
+        void OpenPasswordMouseEnter(object sender, EventArgs e)
         {
             lbL_openPassword.ForeColor = Color.White;
         }
-
-        void OpenPassword_MouseLeave(object sender, EventArgs e)
+        void OpenPasswordMouseLeave(object sender, EventArgs e)
         {
             lbL_openPassword.ForeColor = Color.Black;
         }
-
-        void HidePassword_MouseEnter(object sender, EventArgs e)
+        void HidePasswordMouseEnter(object sender, EventArgs e)
         {
             lbL_hidePassword.ForeColor = Color.White;
         }
-
-        void HidePassword_MouseLeave(object sender, EventArgs e)
+        void HidePasswordMouseLeave(object sender, EventArgs e)
         {
             lbL_hidePassword.ForeColor = Color.Black;
         }
-
-        void Clear_MouseEnter(object sender, EventArgs e)
+        void ClearMouseEnter(object sender, EventArgs e)
         {
             lbL_clear.ForeColor = Color.White;
         }
-
-        void Clear_MouseLeave(object sender, EventArgs e)
+        void ClearMouseLeave(object sender, EventArgs e)
         {
             lbL_clear.ForeColor = Color.Black;
         }
-
         #endregion
 
     }
