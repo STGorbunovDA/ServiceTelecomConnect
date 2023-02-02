af3e8447240d4da377dc810d0857eadc866bd820
STGorbunovDA <bous07@mail.ru>
2023-02-02 11:20:44 +0800

    Refactoring code

--- ServiceTelecomConnect/ServiceTelecomConnect/Forms/Menu.Designer.cs
+++ ServiceTelecomConnect/ServiceTelecomConnect/Forms/Menu.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lbL_section_foreman = new System.Windows.Forms.Label();
+            this.lbL_sectionForeman = new System.Windows.Forms.Label();
             this.lbL_TutorialEngineers = new System.Windows.Forms.Label();
             this.lbL_сomparison = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
@@ -40,18 +40,18 @@
             // 
             // lbL_section_foreman
             // 
-            this.lbL_section_foreman.AutoSize = true;
-            this.lbL_section_foreman.BackColor = System.Drawing.Color.Transparent;
-            this.lbL_section_foreman.Font = new System.Drawing.Font("Courier New", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lbL_section_foreman.Location = new System.Drawing.Point(386, 212);
-            this.lbL_section_foreman.Name = "lbL_section_foreman";
-            this.lbL_section_foreman.Size = new System.Drawing.Size(106, 41);
-            this.lbL_section_foreman.TabIndex = 1;
-            this.lbL_section_foreman.Text = "База";
-            this.lbL_section_foreman.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.lbL_section_foreman.Click += new System.EventHandler(this.Label_baza_Click);
-            this.lbL_section_foreman.MouseEnter += new System.EventHandler(this.Label_section_foreman_MouseEnter);
-            this.lbL_section_foreman.MouseLeave += new System.EventHandler(this.Label_section_foreman_MouseLeave);
+            this.lbL_sectionForeman.AutoSize = true;
+            this.lbL_sectionForeman.BackColor = System.Drawing.Color.Transparent;
+            this.lbL_sectionForeman.Font = new System.Drawing.Font("Courier New", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbL_sectionForeman.Location = new System.Drawing.Point(386, 212);
+            this.lbL_sectionForeman.Name = "lbL_section_foreman";
+            this.lbL_sectionForeman.Size = new System.Drawing.Size(106, 41);
+            this.lbL_sectionForeman.TabIndex = 1;
+            this.lbL_sectionForeman.Text = "База";
+            this.lbL_sectionForeman.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lbL_sectionForeman.Click += new System.EventHandler(this.LblBazaClick);
+            this.lbL_sectionForeman.MouseEnter += new System.EventHandler(this.LblSectionForemanMouseEnter);
+            this.lbL_sectionForeman.MouseLeave += new System.EventHandler(this.LblSectionForemanMouseLeave);
             // 
             // lbL_TutorialEngineers
             // 
@@ -64,9 +64,9 @@
             this.lbL_TutorialEngineers.TabIndex = 2;
             this.lbL_TutorialEngineers.Text = "Обучалка";
             this.lbL_TutorialEngineers.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.lbL_TutorialEngineers.Click += new System.EventHandler(this.Label_TutorialEngineers_Click);
-            this.lbL_TutorialEngineers.MouseEnter += new System.EventHandler(this.Label_TutorialEngineers_MouseEnter);
-            this.lbL_TutorialEngineers.MouseLeave += new System.EventHandler(this.Label_TutorialEngineers_MouseLeave);
+            this.lbL_TutorialEngineers.Click += new System.EventHandler(this.LblTutorialEngineersClick);
+            this.lbL_TutorialEngineers.MouseEnter += new System.EventHandler(this.LblTutorialEngineersMouseEnter);
+            this.lbL_TutorialEngineers.MouseLeave += new System.EventHandler(this.LblTutorialEngineersMouseLeave);
             // 
             // lbL_сomparison
             // 
@@ -79,9 +79,9 @@
             this.lbL_сomparison.TabIndex = 3;
             this.lbL_сomparison.Text = "Для куратора";
             this.lbL_сomparison.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.lbL_сomparison.Click += new System.EventHandler(this.Label1_Click);
-            this.lbL_сomparison.MouseEnter += new System.EventHandler(this.Label_сomparison_MouseEnter);
-            this.lbL_сomparison.MouseLeave += new System.EventHandler(this.Label_сomparison_MouseLeave);
+            this.lbL_сomparison.Click += new System.EventHandler(this.LblComparisonFormClick);
+            this.lbL_сomparison.MouseEnter += new System.EventHandler(this.LblComparisonMouseEnter);
+            this.lbL_сomparison.MouseLeave += new System.EventHandler(this.LblComparisonMouseLeave);
             // 
             // panel1
             // 
@@ -90,7 +90,7 @@
             this.panel1.Controls.Add(this.picB_setting);
             this.panel1.Controls.Add(this.lbL_сomparison);
             this.panel1.Controls.Add(this.lbL_TutorialEngineers);
-            this.panel1.Controls.Add(this.lbL_section_foreman);
+            this.panel1.Controls.Add(this.lbL_sectionForeman);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
@@ -109,9 +109,9 @@
             this.lbL_director.Text = "Руководитель";
             this.lbL_director.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             this.lbL_director.Visible = false;
-            this.lbL_director.Click += new System.EventHandler(this.LbL_director_Click);
-            this.lbL_director.MouseEnter += new System.EventHandler(this.LbL_director_MouseEnter);
-            this.lbL_director.MouseLeave += new System.EventHandler(this.LbL_director_MouseLeave);
+            this.lbL_director.Click += new System.EventHandler(this.LbLDirectorClick);
+            this.lbL_director.MouseEnter += new System.EventHandler(this.LbLDirectorMouseEnter);
+            this.lbL_director.MouseLeave += new System.EventHandler(this.LbLDirectorMouseLeave);
             // 
             // picB_setting
             // 
@@ -125,7 +125,7 @@
             this.picB_setting.TabIndex = 4;
             this.picB_setting.TabStop = false;
             this.picB_setting.Visible = false;
-            this.picB_setting.Click += new System.EventHandler(this.PictureBox1_setting_Click);
+            this.picB_setting.Click += new System.EventHandler(this.SettingClick);
             // 
             // Menu
             // 
@@ -140,9 +140,9 @@
             this.Name = "Menu";
             this.ShowIcon = false;
             this.Text = "Меню";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Menu_FormClosing);
-            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Menu_FormClosed);
-            this.Load += new System.EventHandler(this.Menu_Load);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MenuFormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MenuFormClosed);
+            this.Load += new System.EventHandler(this.MenuLoad);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.picB_setting)).EndInit();
@@ -152,7 +152,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Label lbL_section_foreman;
+        private System.Windows.Forms.Label lbL_sectionForeman;
         private System.Windows.Forms.Label lbL_TutorialEngineers;
         private System.Windows.Forms.Label lbL_сomparison;
         private System.Windows.Forms.Panel panel1;
