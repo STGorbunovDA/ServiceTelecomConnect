a96bdccc076a9ef3c63c934a0088735cd9818147
STGorbunovDA <bous07@mail.ru>
2023-02-05 10:52:47 +0800

    Refactoring code

---
+++ ServiceTelecomConnect/ServiceTelecomConnect/Classes/Cheac/InstanceChecker.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading;
+
+namespace ServiceTelecomConnect
+{
+    class InstanceChecker
+    {
+        static readonly Mutex mutex = new Mutex(false, "ServiceTelecomConnect");
+        public static bool TakeMemory()
+        {
+            return mutex.WaitOne();
+        }
+    }
+}
